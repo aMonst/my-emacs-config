660da6aaedc9e8f065de3b0a43075c592fdc254e
aMonst <liu1793222129@163.com>
2025-03-31 22:00:52 +0800

    :sparkles: 修改一些默认行为

--- init.el
+++ init.el
@@ -3,15 +3,6 @@
 (require 'package-conf)
 (require 'init-ui)
 (require 'init-evil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-themes emacs-rime evil general rime)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
+(load custom-file 'no-error 'no-message)
--- lisp/basic.el
+++ lisp/basic.el
@@ -8,4 +8,18 @@
 (setq display-line-numbers-type 'relative) ;;显示相对行号
 (global-display-line-numbers-mode 1)
 
+(setq make-backup-files nil)                                  ; 不自动备份
+(setq auto-save-default nil)                                  ; 不使用Emacs自带的自动保存
+
+(fset 'yes-or-no-p 'y-pr-n-p)
+(setq confirm-kill-emacs #'y-or-n-p)   ; 在关闭 Emacs 前询问是否确认关闭，防止误触
+(electric-pair-mode t)            ; 自动补全括号
+(column-number-mode t)            ; 在 Mode line 上显示列号
+(global-auto-revert-mode t)         ; 当另一程序修改了文件时，让 Emacs 及时刷新 Buffer
+(delete-selection-mode t)          ; 选中文本后输入文本会替换文本（更符合我们习惯了的其它编辑器的逻辑）
+
+(add-hook 'prog-mode-hook #'hs-minor-mode)  ; 编程模式下，可以折叠代码块
+(add-hook 'prog-mode-hook #'show-paren-mode) ; 编程模式下，光标在括号上时高亮另一个括号
+
+(savehist-mode 1)              ; （可选）打开 Buffer 历史记录保存
 (provide 'basic)
